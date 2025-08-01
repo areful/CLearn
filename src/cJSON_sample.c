5b14cea20b7e924e8287cd2b0dcf43a6d10f5dbd
areful <areful1997@163.com>
2025-08-01 14:52:11 +0800

    opt - CMakeLists.txt

--- src/cJSON_sample.c
+++ src/cJSON_sample.c
@@ -23,7 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../cJSON/cJSON.h"
+#include <cJSON.h>
 
 /* Used by some code below as an example datatype. */
 struct record
